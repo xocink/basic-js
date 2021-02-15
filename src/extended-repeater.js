aa0774b1623a72f994c0d96851a0cd441568e1ab
xocink <57539437+xocink@users.noreply.github.com>
2021-02-15 18:14:18 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,45 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
 
-//     if (typeof str != 'string') {
-//         str = String(str);
-//     }
-//     // options.addition = options.addition ?? '';
-//     if (options.hasOwnProperty('addition')) {
-//         if (typeof options.addition !== 'string') {
-//             options.addition = String(options.addition)
-//         }
-//     }
 
-
-//     options.repeatTimes = options.repeatTimes ?? 1;
-//     options.additionRepeatTimes = options.additionRepeatTimes ?? 1;
-//     options.separator = options.separator ?? '+';
-//     options.additionSeparator = options.additionSeparator ?? '|';
-
-
-//     function getAdditionString() {
-//         let addString = [];
-//         for (let i = 0; i < options.additionRepeatTimes; i++) {
-//             addString.push(options.addition);
-//         }
-//         return addString.join(options.additionSeparator);
-//     };
-
-//     function getExtendedString() {
-//         return str + getAdditionString();
-//     }
-
-
-//     function getFinalString() {
-//         let finalArr = [];
-//         for (let i = 0; i < options.repeatTimes; i++) {
-//             finalArr.push(getExtendedString());
-//         }
-//         return finalArr.join(options.separator);
-//     }
-
-//     return getFinalString();
     // throw new CustomError('Not implemented');
 };
   
