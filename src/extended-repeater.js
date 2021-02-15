4b543aa2ee11fb63fc6c10e382d22e2fb24f45aa
xocink <57539437+xocink@users.noreply.github.com>
2021-02-15 18:18:48 +0300

    fix(??): fix compatibility issues
    Change operator ?? to old version comparison function arguments to null and undefined

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,63 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
+    if (typeof str != 'string') {
+        str = String(str);
+    }
+    // options.addition = options.addition ?? '';
+    if (options.hasOwnProperty('addition')) {
+        if (typeof options.addition !== 'string') {
+            options.addition = String(options.addition)
+        }
+    }
 
+    if (options.repeatTimes !== null && options.repeatTimes !== undefined) {
+        options.repeatTimes = options.repeatTimes
+    } else {
+        options.repeatTimes = 1
+    }
+
+    if (options.additionRepeatTimes !== null && options.additionRepeatTimes !== undefined) {
+        options.additionRepeatTimes = options.additionRepeatTimes
+    } else {
+        options.additionRepeatTimes = 1
+    }
+
+    if (options.separator !== null && options.separator !== undefined) {
+        options.separator = options.separator
+    } else {
+        options.separator = '+'
+    }
+
+    if (options.additionSeparator !== null && options.additionSeparator !== undefined) {
+        options.additionSeparator = options.additionSeparator
+    } else {
+        options.additionSeparator = '|'
+    }
+
+
+    function getAdditionString() {
+        let addString = [];
+        for (let i = 0; i < options.additionRepeatTimes; i++) {
+            addString.push(options.addition);
+        }
+        return addString.join(options.additionSeparator);
+    };
+
+    function getExtendedString() {
+        return str + getAdditionString();
+    }
+
+
+    function getFinalString() {
+        let finalArr = [];
+        for (let i = 0; i < options.repeatTimes; i++) {
+            finalArr.push(getExtendedString());
+        }
+        return finalArr.join(options.separator);
+    }
+
+    return getFinalString();
 
     // throw new CustomError('Not implemented');
 };
