45b63cf60555dec4960f937bb3c92d0af6978773
xocink <57539437+xocink@users.noreply.github.com>
2021-02-18 12:22:30 +0300

    Change extended repeater but still not work properly

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
+
     if (typeof str != 'string') {
         str = String(str);
     }
