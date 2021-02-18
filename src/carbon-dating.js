bfd8517043eaa2f3d083ca8f291a7915dcd70b63
xocink <57539437+xocink@users.noreply.github.com>
2021-02-18 12:20:54 +0300

    Add crabon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,21 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+      if (arguments.length === 0 || typeof sampleActivity !== 'string') {
+        return false;
+    }
+    sampleActivity = +sampleActivity;
+    if (sampleActivity <= 0 || sampleActivity >= 15 ){
+        return false;
+    }
+    if (isNaN(sampleActivity)) {
+        return false;
+    }
+
+    let rate = 0.693 / HALF_LIFE_PERIOD;
+
+    let age = Math.log(MODERN_ACTIVITY / sampleActivity) / rate;
+    return Math.ceil(age)
+//   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
